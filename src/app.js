8c2b12aae2b5538f57a6a3112b4aa69818d552f7
Ayush Singh <ayushsingh638684@gmail.com>
2026-05-01 00:38:28 +0530

    file fixes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/' , (req,res) =>{
-    res.status(200).send("hello from Acquisitions API");
-})
+app.get('/', (req, res) => {
+  res.status(200).send('hello from Acquisitions API');
+});
 
 export default app;
-
-
--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
 import app from './app.js';
 import dotenv from 'dotenv';
 
-
 const PORT = process.env.PORT || 3000;
 
-
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+  res.send('Hello, World!');
 });
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
