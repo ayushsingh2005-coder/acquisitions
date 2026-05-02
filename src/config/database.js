761576e3ec76670d9f8e216e6a59ae974bf58a75
Ayush Singh <ayushsingh638684@gmail.com>
2026-05-02 17:04:58 +0530

    implement auth

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 
 import {neon} from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
+import * as schema from '#models/user.model.js';
 
 // initialzing neon client
 const sql = neon(process.env.DATABASE_URL);
