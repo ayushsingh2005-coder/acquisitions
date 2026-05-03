6faf083d9ebac9ba517d8ff9dc4f300da5bea73d
Ayush Singh <ayushsingh638684@gmail.com>
2026-05-04 00:24:41 +0530

    Testing done with Jest and files added

--- src/app.js
+++ src/app.js
@@ -34,10 +34,14 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/api' , (req,res) =>{
-    res.status(200).json({message : 'Acquisitions API us running!'})
+    res.status(200).json({message : 'Acquisitions API is running'})
 })
 
 app.use('/api/auth' , authRoutes);
 app.use('/api/users' , usersRoutes);
+
+app.use((req ,res) => {
+    res.status(404).json({error : 'Route not found'});
+})
 
 export default app;
